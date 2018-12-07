1aa6cb6176b53eba1ca58cf4017ba633684f066c
Caio Vinícius <viniciuscaio.17@gmail.com>
2018-12-07 10:39:53 -0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:intro_scaffold2/ui/home.dart';
+
+void main() {
+
+  runApp(MaterialApp(
+    title: "Scaffold",
+    home: Home()
+  ));
+}
