cb161db5bee5e4c6857c599ff773b52a76325025
Caio Vinícius <viniciuscaio.17@gmail.com>
2018-12-07 12:18:14 -0200

    Projeto completo da seção 14

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -2,13 +2,73 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
 
+  void _onPress() {
+
+    print("Search tapped!");
+  }
+
   @override
   Widget build(BuildContext context) {
     
-    return Scaffold(
-      appBar: AppBar(
+    return Scaffold (
+      appBar: AppBar (
         backgroundColor: Colors.indigoAccent,
-        title: Text("Fency Day")
+        title: Text ("Fency Day"),
+        actions: <Widget>[
+          IconButton (
+            icon: Icon(Icons.send),
+            onPressed: () => debugPrint("send icon tapped!")
+          ),
+          IconButton (
+            icon: Icon(Icons.search),
+            onPressed: _onPress,
+            )
+        ]
+      ),
+      backgroundColor: Colors.blueGrey.shade200,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Bonni",
+              style: TextStyle(
+                fontSize: 14.5,
+                fontWeight: FontWeight.w400,
+                color: Colors.deepOrange
+              )
+            ),
+            InkWell(
+              child: Text("button"),
+              onTap: () => debugPrint("Button tapped!")
+            )
+          ]
+        )
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => debugPrint("Pressed!"),
+        backgroundColor: Colors.white12,
+        tooltip: "Going up!",
+        child: Icon(Icons.check)
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            title: Text("Hey!")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.print),
+            title: Text("Nope!")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call_missed),
+            title: Text("Hello!")
+          )
+        ],
+        onTap: (int i) => debugPrint("Touched index $i")
       )
     );
   }
